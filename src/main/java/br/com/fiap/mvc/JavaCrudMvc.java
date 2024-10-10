89dd0b96600cd055d9d28f88764245aa94862967
diogo_gallina <diogogallina20@gmail.com>
2024-10-09 21:40:57 -0300

    chore: add config project

---
+++ src/main/java/br/com/fiap/mvc/JavaCrudMvc.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudMvc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudMvc.class, args);
+	}
+
+}
